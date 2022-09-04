5f3f141b15fd42f23d11775ecad7ed332996f792
TomtenPim <77719357+TomtenPim@users.noreply.github.com>
2022-09-04 10:58:46 +0200

    tror jag sitter fast, spottar fortfarande bara ut info om själva arrayen

--- src/kryptering.java
+++ src/kryptering.java
@@ -2,16 +2,34 @@ import javax.swing.*;
 
 public class kryptering {
     public static void main(String[] args) {
-        //String input = JOptionPane.showInputDialog(null,"Det som ska krypteras här tack");
-        //String key = JOptionPane.showInputDialog(null,"Nyckel här tack");
 
-        int input = Integer.parseInt(JOptionPane.showInputDialog(null,"Det som ska krypteras här tack"));
-        int key = Integer.parseInt(JOptionPane.showInputDialog(null,"Nyckel här tack"));
-        System.out.println(Encrypt(input,key));
+        //int input = Integer.parseInt(JOptionPane.showInputDialog(null,"Det som ska krypteras här tack"));
+        //int key = Integer.parseInt(JOptionPane.showInputDialog(null,"Nyckel här tack"));
 
+        String input = JOptionPane.showInputDialog(null,"Det som ska krypteras här tack");
+        String key = JOptionPane.showInputDialog(null,"Nyckel här tack");
+        String output = "";
+        int[] hexInput = new int[input.length()];
+        int[] hexKey = new int[key.length()];
+
+        for(int i = 0; i<input.length() ; i++){
+            hexInput[i] = input.charAt(i);
+        }
+
+        for(int i = 0; i<key.length() ; i++){
+            hexKey[i] = key.charAt(i);
+        }
+        output += (Encrypt(input.length(),hexInput,hexKey));
+        System.out.println(output);
     }
-    static int Encrypt(int input, int key){
-        int output = input^key;
+
+    static int[] Encrypt(int length, int hexInput[], int hexKey[]){
+
+        int[] output = new int[length];
+
+        for(int i=0; i > length; i++){
+        output[i] += hexInput[i] ^ hexKey[i];
+        }
         return output;
     }
 }
