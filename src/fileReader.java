a4a3310f51807da7b7743fd807125453d7c943a1
TomtenPim <77719357+TomtenPim@users.noreply.github.com>
2022-09-08 13:44:41 +0200

    kryptering, klarar nu korta keys FileReader, har ingen aning vad jag gör eller varför

---
+++ src/fileReader.java
@@ -0,0 +1,45 @@
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+
+public class fileReader {
+    public static void main(String[] args) {
+        BufferedReader inFile = openTextFileForReading("info.text");
+        ArrayList<String> fileContent = readAllText(inFile);
+        String file = "file.txt";
+        DataOutputStream outFile = openBinaryFileWriting("info.dat");
+        writeTextAsBinary(fileContent, outFile);
+    }
+
+    private static void writeTextAsBinary(ArrayList<String> fileContent) {
+        for (int i = 0; i < fileContent.size(); i++){
+            try{
+                outFile.write(fileContent.get(i).getBytes());
+            } catch (IOException e) {
+
+            }
+        }
+    }
+
+    private static ArrayList<String> readAllText(BufferedReader inFile) {
+        ArrayList<String> text = new ArrayList<>();
+        try{
+            String line = inFile.readlLine();
+            while(line != null) {
+                text.add(line);
+                line = inFile.readLine();
+            }
+        }
+    }
+
+    private static BufferedReader openTextFileForReading(String file) {
+        BufferedReader in = null;
+        try {
+            in = new BufferedReader(new FileReader(file));
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found, use keyboard instead!");
+            ;
+        }
+        return in;
+    }
+}
--- src/kryptering.java
+++ src/kryptering.java
@@ -3,22 +3,21 @@ import javax.swing.*;
 public class kryptering {
     public static void main(String[] args) {
 
-        //int input = Integer.parseInt(JOptionPane.showInputDialog(null,"Det som ska krypteras här tack"));
-        //int key = Integer.parseInt(JOptionPane.showInputDialog(null,"Nyckel här tack"));
-
         String input = JOptionPane.showInputDialog(null,"Det som ska krypteras här tack");
         String key = JOptionPane.showInputDialog(null,"Nyckel här tack");
         int[] Input = new int[input.length()];
         int[] Output = new int[input.length()];
-        int[] Key = new int[key.length()];
+        int[] Key = new int[input.length()];
 
         for(int i = 0; i<input.length() ; i++){
+            int keyWrap = (i/key.length());
             Input[i] = input.charAt(i);
+            Key[i] = key.charAt(i-(keyWrap*key.length()));
         }
 
         for(int i = 0; i<key.length() ; i++){
-            Key[i] = key.charAt(i);
         }
+
 
         for(int i = 0; i<input.length(); i++){
            Output[i] += (Encrypt(i,Input,Key));
