0181eed740751f02141637059ee17af3e20b0511
TomtenPim <77719357+TomtenPim@users.noreply.github.com>
2022-09-29 13:43:01 +0200

    binary fixad real

--- src/fileReaderOcringe.java
+++ src/fileReaderOcringe.java
@@ -1,29 +1,34 @@
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
 import java.util.Scanner;
 
 public class fileReaderOcringe {
     public static void main(String[] args) {
 
         int [] number = new int [2073155];
-        int [] Output = new int [2073155];
 
+        //Scannar in varje rad i Random.txt och skriver in dem i Integern number
         try{
-        Scanner scanner = new Scanner(new File("Random.txt"));
-        int i = 0;
+            Scanner scanner = new Scanner(new File("Random.txt"));
+            int i = 0;
 
-        while(scanner.hasNextInt())
-        {
-            number[i++] = scanner.nextInt();
-        }
+            while(scanner.hasNextInt()) {
+                number[i++] = scanner.nextInt();
+            }
         } catch (IOException e){
             System.out.println("An error occurred.");
             e.printStackTrace();
         }
 
+        //Skep
+        try {
+            DataOutputStream output = new DataOutputStream(new BufferedOutputStream(new FileOutputStream("Binary.txt")));
         for (int i=0; i < number.length; i++){
-            String s = Integer.toBinaryString(number[i]);
-            Output [i] += Integer.parseInt(s);
+                   output.writeInt(number[i]);
+        }
+            } catch (IOException e) {
+                System.out.println("An error occurred.");
+                e.printStackTrace();
+            }
         }
     }
-}
+
