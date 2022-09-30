0b282fbfb6b27c166de42c6f2ddf63cea52a7785
TomtenPim <77719357+TomtenPim@users.noreply.github.com>
2022-09-30 13:13:16 +0200

    inital commit Krypteringsuppgift

---
+++ src/krypteringCool.java
@@ -0,0 +1,13 @@
+public class krypteringCool {
+    public static void main(String[] args) {
+
+    }
+
+    public static void View(String[] args) {
+
+    }
+
+    public static void Model(String[] args){
+
+    }
+}
