5f098a6149b2f071b5a726c319464d4853f5a6bc
TomtenPim <77719357+TomtenPim@users.noreply.github.com>
2022-10-13 13:17:25 +0200

    controller påbörjad och model omkonstruering påbörjad

--- src/MVCKryptreing/KrypteringControler.java
+++ src/MVCKryptreing/KrypteringControler.java
@@ -3,8 +3,34 @@ package MVCKryptreing;
 import javax.swing.*;
 
 public class KrypteringControler {
-    public static void main(String[] args) {
+    private KrypteringView theView;
+    private KrypteringModel theModel;
 
+    public KrypteringControler(KrypteringView theView, KrypteringModel theModel) {
+        this.theView = theView;
+        this.theModel = theModel;
+        // Tell the View that when ever the calculate button
+        // is clicked to execute the actionPerformed method
+        // in the CalculateListener inner class
+        this.theView.addCalculateListener(new CalculateListener());
     }
 
+    private class CalculateListener implements ActionListener{
+        public void actionPerformed(ActionEvent e) {
+            int firstNumber, secondNumber = 0;
+            // Surround interactions with the view with
+            // a try block in case numbers weren't
+            // properly entered
+            try{
+                firstNumber = theView.getFirstNumber();
+                secondNumber = theView.getSecondNumber();
+                theModel.addTwoNumbers(firstNumber, secondNumber);
+                theView.setCalcSolution(theModel.getCalculationValue());
+            }
+            catch(NumberFormatException ex){
+                System.out.println(ex);
+                theView.displayErrorMessage("You Need to Enter 2 Integers");
+            }
+        }
+    }
 }
--- src/MVCKryptreing/KrypteringModel.java
+++ src/MVCKryptreing/KrypteringModel.java
@@ -5,18 +5,22 @@ import javax.swing.*;
 public class KrypteringModel {
     public static void main(String[] args) {
 
+        /*
+
         String input = JOptionPane.showInputDialog(null,"Det som ska krypteras här tack");
         String key = JOptionPane.showInputDialog(null,"Nyckel här tack");
         int[] Input = new int[input.length()];
         int[] Output = new int[input.length()];
         int[] Key = new int[input.length()];
 
+        //key-förlängare för kryptering och array placering av key och text
         for(int i = 0; i<input.length() ; i++){
             int keyWrap = (i/key.length());
             Input[i] = input.charAt(i);
             Key[i] = key.charAt(i-(keyWrap*key.length()));
         }
 
+        //call-ar encrypt-funktionen för varje tecken och krypterar det
         for(int i = 0; i<input.length(); i++){
             Output[i] += (Encrypt(i,Input,Key));
             System.out.print((char)(Output)[i]);
@@ -24,8 +28,11 @@ public class KrypteringModel {
         System.out.println("");
     }
 
+    //faktiskt krypterar
     static int Encrypt(int i, int Input[], int Key[]){
         int out = Input[i] ^ Key[i];
         return out;
+
+         */
     }
 }
