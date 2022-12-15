f0f815717c2c58565b4689b5419414b09dfb7f4c
TomtenPim <77719357+TomtenPim@users.noreply.github.com>
2022-12-15 13:29:31 +0100

    start på Model GoL samt importera filerna

---
+++ src/GameOfLife/Model.java
@@ -0,0 +1,23 @@
+package GameOfLife;
+
+import java.awt.*;
+
+public class Model {
+    int height;
+    int width;
+
+    public Model(int height, int width){
+
+    }
+
+    public void update(){
+
+    }
+
+    public Shape[] getShapes(){
+        Point[] points = {new Point(5,5)};
+        return (Shape[])points;
+    }
+
+
+}
