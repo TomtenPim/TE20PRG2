a43a3e75eb7d0e4a8c151e89c5bed51ee7f16cb4
TomtenPim <77719357+TomtenPim@users.noreply.github.com>
2023-04-20 12:56:45 +0200

    Creature base

---
+++ src/Cardgame/Creature.java
@@ -0,0 +1,15 @@
+package Cardgame;
+
+public class Creature {
+    int str;
+    int dex;
+    int con;
+    int inte;
+    int wis;
+    int cha;
+    int ac;
+    int hp;
+    int movement;
+    String proficiency;
+    int proficiencyBonus;
+}
